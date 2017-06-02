b0cffd6302ec8d7a346f6aa2bf22c0d48885164f
Roderick Braun <roderick.braun@ph-freiburg.de>
2017-06-02 10:51:13 +0200

    Update shibd-conf.d

--- www-apps/shibboleth-sp/files/shibd-conf.d
+++ www-apps/shibboleth-sp/files/shibd-conf.d
@@ -1,5 +1,7 @@
 # Copyright 1999-2011 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 
-SHIBD_OPTS="-f -w 1 -p /var/run/shibboleth/shibd.pid"
+SHIBD_PIDDIR="/var/run/shibboleth"
+SHIBD_PIDFILE="shibd.pid"
 
+SHIBD_OPTS="-F -f -w 1 -c /etc/shibboleth/shibboleth2.xml"
