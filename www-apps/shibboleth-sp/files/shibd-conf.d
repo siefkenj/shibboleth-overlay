28f41c46f615fba5155bdefb23f7fbd1eb638598
Roderick Braun <roderick.braun@ph-freiburg.de>
2017-10-17 13:25:57 +0200

    Update shibd-conf.d

--- www-apps/shibboleth-sp/files/shibd-conf.d
+++ www-apps/shibboleth-sp/files/shibd-conf.d
@@ -2,6 +2,9 @@
 # Distributed under the terms of the GNU General Public License v2
 
 SHIBD_PIDDIR="/var/run/shibboleth"
+SHIBD_CACHEDIR="/var/cache/shibboleth"
 SHIBD_PIDFILE="shibd.pid"
+SHIBD_USER="shibd"
+SHIBD_GROUP="shibd"
 
 SHIBD_OPTS="-F -f -w 1 -c /etc/shibboleth/shibboleth2.xml"
