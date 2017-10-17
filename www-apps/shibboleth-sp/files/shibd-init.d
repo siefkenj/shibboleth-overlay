369308873acb1af6e05653389b67a31aa4b0eab2
Roderick Braun <roderick.braun@ph-freiburg.de>
2017-10-17 13:26:49 +0200

    Update shibd-init.d

--- www-apps/shibboleth-sp/files/shibd-init.d
+++ www-apps/shibboleth-sp/files/shibd-init.d
@@ -1,22 +1,23 @@
 #!/sbin/openrc-run
 
 depend() {
-	need net
-	use apache
+        need net
+        use apache
 }
 
 start() {
-	ebegin "Starting Shibboleth Service Provider Daemon"
-	  mkdir -p ${SHIBD_PIDDIR}
-	  start-stop-daemon --start --pidfile ${SHIBD_PIDDIR}/${SHIBD_PIDFILE} \
-	    --background --make-pidfile --exec /usr/sbin/shibd -- ${SHIBD_OPTS}
-	eend $?
+        ebegin "Starting Shibboleth Service Provider Daemon"
+          mkdir -p ${SHIBD_PIDDIR}
+          chown -R ${SHIBD_USER}:${SHIBD_GROUP} ${SHIBD_PIDDIR} ${SHIBD_CACHEDIR}
+          start-stop-daemon --start --pidfile ${SHIBD_PIDDIR}/${SHIBD_PIDFILE} \
+            --background --make-pidfile --exec /usr/sbin/shibd -- ${SHIBD_OPTS} -u ${SHIBD_USER} -g ${SHIBD_GROUP}
+        eend $?
 
 }
 
 stop() {
-	ebegin "Stopping Shibboleth Service Provider Daemon"
-	  start-stop-daemon --stop --pidfile ${SHIBD_PIDDIR}/${SHIBD_PIDFILE} --exec \
-	    /usr/sbin/shibd
-	eend $?
+        ebegin "Stopping Shibboleth Service Provider Daemon"
+          start-stop-daemon --stop --pidfile ${SHIBD_PIDDIR}/${SHIBD_PIDFILE} --exec \
+            /usr/sbin/shibd
+        eend $?
 }
